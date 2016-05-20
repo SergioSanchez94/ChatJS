b6d6bea7154ff2e8f9c29815151c9bd728dc7b82
sergio.sanchezc <sergio.sanchezc@28APO1891.altran.es>
2016-05-20 14:38:11 +0200

    Diseño Mapas

--- ChatJS/public/controllers/chatController.js
+++ ChatJS/public/controllers/chatController.js
@@ -130,6 +130,10 @@ angular.module('app').controller("MainController", function($scope, $window, $ht
 							return ('<div id="'+elem._id+'" class="mapMio" style="height: 50%; width: 50%;"></div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>');
 						}
 					}
+					if(elem.tipo == "file"){
+						//Archivo
+						console.log("Archivo encontrado");
+					}
 				}).join("");
 		
 		document.getElementById('messages').innerHTML = html;
@@ -155,10 +159,14 @@ angular.module('app').controller("MainController", function($scope, $window, $ht
 			
 			var myLatLng = {lat : parseFloat(coords[0]), lng : parseFloat(coords[1])};
 			
+			var styled = [{"featureType":"administrative.country","elementType":"labels.text","stylers":[{"lightness":"29"}]},{"featureType":"administrative.province","elementType":"labels.text.fill","stylers":[{"lightness":"-12"},{"color":"#796340"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"lightness":"15"},{"saturation":"15"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#fbf5ed"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#fbf5ed"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"},{"lightness":"30"},{"saturation":"-41"},{"gamma":"0.84"}]},{"featureType":"poi.attraction","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.medical","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#b0e9ac"},{"visibility":"on"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"hue":"#68ff00"},{"lightness":"-24"},{"gamma":"1.59"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","elementType":"geometry","stylers":[{"saturation":"10"},{"color":"#c3eb9a"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"lightness":"30"},{"color":"#e7ded6"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"on"},{"saturation":"-39"},{"lightness":"28"},{"gamma":"0.86"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe523"},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"saturation":"0"},{"gamma":"1.44"},{"color":"#fbc28b"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"},{"saturation":"-40"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#fed7a5"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"gamma":"1.54"},{"color":"#fbe38b"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"visibility":"on"},{"gamma":"2.62"},{"lightness":"10"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"weight":"0.50"},{"gamma":"1.04"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#dee3fb"}]},{"featureType":"water","elementType":"geometry","stylers":[{"saturation":"46"},{"color":"#a4e1ff"}]}];
+			
 			var map = new google.maps.Map(document.getElementById(map[0]), {
 				center : myLatLng,
 				zoom : 15
 			});
+			
+			map.setOptions({styles: styled});
 			
 			var image = {
 				    url: 'img/location_icon.png',
@@ -530,5 +538,15 @@ angular.module('app').controller("MainController", function($scope, $window, $ht
 
 	    return text;
 	}
-
+	
+	$scope.sendLocation = function(){
+		if (navigator.geolocation) {
+		    navigator.geolocation.getCurrentPosition(function(position) {
+		        var text = position.coords.latitude + "," + position.coords.longitude;
+		        console.log("Location: " + text);
+		      }
+		    );
+		}
+	}
+	
 });
